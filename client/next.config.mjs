57292bbd66a896782ee13b79c5bef6abf3663321
dibyaadhikaree <dibyaaadhikari@gmail.com>
2026-03-22 02:07:44 +0545

    changed config .js

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,19 +1,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  async rewrites() {
-    const backendBaseUrl =
-      process.env.API_PROXY_TARGET ||
-      process.env.NEXT_PUBLIC_API_BASE_URL ||
-      "http://localhost:5000";
-
-    return [
-      {
-        source: "/backend/:path*",
-        destination: `${backendBaseUrl}/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
